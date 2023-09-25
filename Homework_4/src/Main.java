8cda4524a9253072f450529309fd235ac58dd90f
Kio <kio.m@gmail.com>
2023-09-25 08:42:46 +0300

    Homework_4

--- Homework_4/src/Main.java
+++ Homework_4/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
-        int T = 0;
+        int T = 20;
         if (T < 17)
             System.out.println("hit");
-
+        else if (T > 16 && T < 22)
+            System.out.println("stay");
+        else
+            System.out.println("bust");
     }
 }
